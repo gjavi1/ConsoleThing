2919e5af6e6b6fdf73a307422da287d74d36a4f9
Glen Javier <glen02javier@gmail.com>
2017-08-25 23:56:01 +1000

    added super basic console component

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,40 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Console from './Console/Console'
+
 class App extends Component {
+    constructor(props) {
+        super(props);
+  
+        this.state = {
+          count: 0,
+        };
+    }
+
+    child = {
+        console: Console
+    }
+  
+    echo = (text) => {
+        this.child.console.log(text);
+        this.setState({
+          count: this.state.count+1,
+        }, this.child.console.return);
+    }
+    
+    promptLabel = () => {
+        return this.state.count + "> ";
+    }
+    
     render() {
-        return (
-            <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h2>Welcome to React</h2>
-                </div>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
-            </div>
-        );
+        return <Console ref={ref => this.child.console = ref}
+            handler={this.echo}
+            promptLabel={this.promptLabel}
+            welcomeMessage={"ConsoleThingy"}
+            autofocus={true}
+            promptLabel= {'> '}
+        />;
     }
 }
 
